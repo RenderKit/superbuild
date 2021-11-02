e11957b32064a79ec26a717e90ceee0c5768e085
Trevor Thomson <trevor.thomson@intel.com>
2021-11-02 15:54:39 -0400

    Update version numbers for Q4 2021

--- get_glfw.cmake
+++ get_glfw.cmake
@@ -1,7 +1,7 @@
 ## Copyright 2009-2021 Intel Corporation
 ## SPDX-License-Identifier: Apache-2.0
 
-set(GLFW_VERSION 3.2.1)
+set(GLFW_VERSION 3.3.4)
 
 set(COMPONENT_NAME glfw)
 
--- get_tbb.cmake
+++ get_tbb.cmake
@@ -1,7 +1,7 @@
 ## Copyright 2021 Intel Corporation
 ## SPDX-License-Identifier: Apache-2.0
 
-set(TBB_VERSION 2021.2.0)
+set(TBB_VERSION 2021.4.0)
 
 set(SUBPROJECT_NAME oneapi-tbb-${TBB_VERSION})
 
